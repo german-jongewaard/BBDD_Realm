e90917fe9ca019ce5bda27c673d1252a623446ef
german <germanjongewaard@gmail.com>
2017-12-08 18:06:36 +0100

    BBDD Realm

--- app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/BoardActivity.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/BoardActivity.java
@@ -1,5 +1,6 @@
 package dev.com.jongewaard.bbdd_realm.activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import dev.com.jongewaard.bbdd_realm.R;
 
 public class BoardActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        fab = (FloatingActionButton) findViewById(R.id.fabAddBoard);
     }
 }
