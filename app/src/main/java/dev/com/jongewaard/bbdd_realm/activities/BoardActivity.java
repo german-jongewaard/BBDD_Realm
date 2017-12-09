1631bed06ef681ebe94d203494f3a3c1d18e1fb1
german <germanjongewaard@gmail.com>
2017-12-09 01:51:50 +0100

    BBDD Realm

--- app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/BoardActivity.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/BoardActivity.java
@@ -1,9 +1,14 @@
 package dev.com.jongewaard.bbdd_realm.activities;
 
+import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import dev.com.jongewaard.bbdd_realm.R;
 
@@ -24,8 +29,32 @@ public class BoardActivity extends AppCompatActivity {
         //creo una instancia del Builder, es un Pop-pup que va a tener un Input
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
+        if(title != null) builder.setTitle(title);
+        if(message != null) builder.setMessage(message);
+
+        //inflo la vista
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_board, null);
+        builder.setView(viewInflated);//le paso la lista para que la embeba
+
+        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewBoard);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //trim() es para borrar los espacios al final, por si el usuario los dejara
+                String boardName = input.getText().toString().trim();
+                if(boardName.length() > 0)
+                        createNewBoard(boardName);
+                    else
+                    Toast.makeText(getApplicationContext(), "The name is required to create a new Board", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
+
+    }
+
+    private void createNewBoard(String boardName) {
 
     }
 }
