8c28c4cbe07cd2b00210a269c2d4189ca2d16a27
german <germanjongewaard@gmail.com>
2017-12-11 14:02:05 +0100

    BBDD Realm

---
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/BoardAdapter.java
@@ -0,0 +1,38 @@
+package dev.com.jongewaard.bbdd_realm.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by german on 11-12-17.
+ */
+
+public class BoardAdapter extends BaseAdapter {
+
+
+
+
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
