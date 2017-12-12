6a7048e01a8c8e61027ed72458b31c53f01b0f1d
german <germanjongewaard@gmail.com>
2017-12-12 02:32:13 +0100

    BBDD Realm

--- app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/NoteAdapter.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/NoteAdapter.java
@@ -1,8 +1,40 @@
 package dev.com.jongewaard.bbdd_realm.adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 /**
  * Created by german on 12-12-17.
  */
 
-public class NoteAdapter {
+public class NoteAdapter extends BaseAdapter {
+
+
+
+
+
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return convertView;
+    }
 }
