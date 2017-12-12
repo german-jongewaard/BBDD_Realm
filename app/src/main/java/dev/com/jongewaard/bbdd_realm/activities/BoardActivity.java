6f05bee7023b45c576d0238d54c2d11224d2682c
german <germanjongewaard@gmail.com>
2017-12-13 00:45:36 +0100

    BBDD Realm

--- app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/BoardActivity.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/BoardActivity.java
@@ -6,7 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -21,7 +24,7 @@ import io.realm.Realm;
 import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 
-public class BoardActivity extends AppCompatActivity implements RealmChangeListener<Board>, AdapterView.OnItemClickListener{
+public class BoardActivity extends AppCompatActivity implements RealmChangeListener<RealmResults<Board>>, AdapterView.OnItemClickListener{
 
     private Realm realm;
 
@@ -40,14 +43,12 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
         // DbRealm
         realm = Realm.getDefaultInstance();
         boards = realm.where(Board.class).findAll();
-        boards.addChangeListener((OrderedRealmCollectionChangeListener<RealmResults<Board>>) this);
-
-
-
+        boards.addChangeListener(this);
 
         adapter = new BoardAdapter(this, boards, R.layout.list_view_board_item);
         listView = (ListView) findViewById(R.id.listViewBoard);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
 
         fab = (FloatingActionButton) findViewById(R.id.fabAddBoard);
 
@@ -59,7 +60,17 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
                 showAlertForCreatingBoard("Aa listadd New Board", "Type a name for your new board");
             }
         });
+
+        /* Borra toda la BBDD
+        realm.beginTransaction();
+        realm.deleteAll();
+        realm.commitTransaction();
+        */
+        //aqui registro la vista, sin este paso no se puede ver las opciones de borrar y editar
+        registerForContextMenu(listView);
+
     }
+
 
 
     //** CRUD Actions **/ Seccion 7 Clase 80
@@ -72,11 +83,35 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
                 realm.copyToRealm(board);
             }
         });
+    }
 
+    //metodo para editar una nota
+    private void editBoard(String newName, Board board){
+        realm.beginTransaction();
+        board.setTitle(newName); //cojo el Titulo
+        realm.copyToRealmOrUpdate(board); //copio y lo actualizo
+        realm.commitTransaction();
+
+    }
+
+    //metodo para borrar una Nota
+    private void deleteBoard(Board board){
+
+        realm.beginTransaction();//Borro toda la BBDD
+        board.deleteFromRealm();//con este metodo borramos desde la BBDD
+        realm.commitTransaction();
+    }
+
+    //metodo para borrar toda la BBDD
+    private void deleteAll(){
+        realm.beginTransaction();//Borro toda la BBDD
+        realm.deleteAll();
+        realm.commitTransaction();
     }
 
 
     //** Dioalogs **/
+
     //elemento usado en MaterialDesign
     private void showAlertForCreatingBoard(String title, String message){
         //creo una instancia del Builder, es un Pop-pup que va a tener un Input
@@ -94,12 +129,12 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
         builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                //trim() es para borrar los espacios al final, por si el usuario los dejara
+                //trim() es para borrar l AdapterView.AdapterContextMenuInfo  info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo(); //aqui tengo la información ya..os espacios al final, por si el usuario los dejara
                 String boardName = input.getText().toString().trim();
                 if(boardName.length() > 0)
                         createNewBoard(boardName);
                     else
-                    Toast.makeText(getApplicationContext(), "The name is required to create a new Board", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "The name is required to create a new Board", Toast.LENGTH_LONG).show();
             }
         });
         //aqui lo crea y lo enseña! (a la alerta)
@@ -107,12 +142,114 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
         dialog.show();
     }
 
+    /* Aqui muestro mensaje de la EDICION */
+    private void showAlertForEditingBoard(String title, String message, final Board board){
+        //creo una instancia del Builder, es un Pop-pup que va a tener un Input
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if(title != null) builder.setTitle(title);
+        if(message != null) builder.setMessage(message);
+
+        //inflo la vista
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_board, null);
+        builder.setView(viewInflated);//le paso la lista para que la embeba
+
+        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewBoard);
+        input.setText(board.getTitle()); // cuando voy a editar muestra el texto de lo que quiero editar
+
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //trim() es para borrar los espacios al final, por si el usuario los dejara
+                String boardName = input.getText().toString().trim();
+                if(boardName.length() == 0)
+                    Toast.makeText(getApplicationContext(), "The name is required to create a new Board", Toast.LENGTH_LONG).show();
+                else if(boardName.equals(board.getTitle()))
+                    Toast.makeText(getApplicationContext(), "The name is the same than is was before", Toast.LENGTH_LONG).show();
+                else
+                    editBoard(boardName, board); //aqui edita
+
+
+            }
+        });
+        //aqui lo crea y lo enseña! (a la alerta)
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    /* Events */
+
     @Override
-    public void onChange(Board board) {
-        /* Esto es para refrescar el adaptado, cada vez que se lanza el evento el cual
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_board_activity, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //tengo que sobrescribir el metodo que controlaba los eventos
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.delete_all:
+                deleteAll();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+            AdapterView.AdapterContextMenuInfo  info = (AdapterView.AdapterContextMenuInfo)menuInfo; //aqui tengo la información ya..
+            menu.setHeaderTitle(boards.get(info.position).getTitle()); // aqui paso el titulo
+            getMenuInflater().inflate(R.menu.context_menu_board_activity, menu);//aqui lo inflo!
+    }
+
+
+    //aqui manejo los dos eventos, el borrar solamente una nota y el editar una nota.
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo  info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo(); //aqui tengo la información ya..
+
+        switch (item.getItemId()){
+
+            case R.id.delete_board:
+
+                deleteBoard(boards.get(info.position));//aqui ya puedo borrar y esta implementado
+
+                return true;
+
+            case R.id.edit_board:
+
+                showAlertForEditingBoard("Edit Board", "Change the name of the board", boards.get(info.position));
+
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onChange(RealmResults<Board> boards) {
+         /* Esto es para refrescar el adaptado, cada vez que se lanza el evento el cual
         conlleva un cambio en la lista del resultado, refresca el adaptador  */
         adapter.notifyDataSetChanged();
     }
+
 
     /* Luego de implementar en la clase el: AdapterView.OnItemClickListener. genero el onItemClick
      * para que sea usado para el ListView que tenemos*/
--- app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/NoteActivity.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/activities/NoteActivity.java
@@ -18,10 +18,11 @@ import dev.com.jongewaard.bbdd_realm.models.Board;
 import dev.com.jongewaard.bbdd_realm.models.Note;
 import io.realm.OrderedRealmCollectionChangeListener;
 import io.realm.Realm;
+import io.realm.RealmChangeListener;
 import io.realm.RealmList;
 import io.realm.RealmResults;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements RealmChangeListener<Board> {
 
     private Realm realm;
 
@@ -45,6 +46,7 @@ public class NoteActivity extends AppCompatActivity {
         if(getIntent().getExtras() != null) boardId = getIntent().getExtras().getInt("id");
 
         board = realm.where(Board.class).equalTo("id", boardId).findFirst();
+        board.addChangeListener(this);
         notes = board.getNotes();
 
         this.setTitle(board.getTitle());
@@ -59,13 +61,48 @@ public class NoteActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAlertForCreatingNote("Aa listadd New Board", "Type a name for your new board");
+                showAlertForCreatingNote("Add new Note", "Type a note for " + board.getTitle() + ".");
             }
         });
-
      }
 
-    //** Dioalogs **/
+            /* CRUD Actions */
+
+    //** CRUD Actions **/ Seccion 7 Clase 80
+    private void createNewNote(final String note) {
+
+        realm.beginTransaction();//empieza
+
+        Note _note  = new Note(note);
+        realm.copyToRealm(_note);//esta linea guarda la nota en la BBDD
+        board.getNotes().add(_note);//aqui añado a la pizarra la nota que he creado
+
+        realm.commitTransaction();//termina
+
+    }
+
+    private void editNote(String newNoteDescription, Note note){
+        realm.beginTransaction();//empieza
+        note.setDescription(newNoteDescription); //escribe en la nota
+        realm.copyToRealmOrUpdate(note); //copia y actualiza la nota
+        realm.commitTransaction();//termina
+    }
+
+    private void deleteNote(Note note){
+        realm.beginTransaction();//empieza
+        note.deleteFromRealm();
+        realm.commitTransaction();//termina
+    }
+
+    private void deleteAll(){
+        realm.beginTransaction();
+        realm.deleteAll();
+        realm.commitTransaction();
+    }
+
+
+        //** Dioalogs **/
+
     //elemento usado en MaterialDesign
     private void showAlertForCreatingNote(String title, String message){
         //creo una instancia del Builder, es un Pop-pup que va a tener un Input
@@ -75,25 +112,32 @@ public class NoteActivity extends AppCompatActivity {
         if(message != null) builder.setMessage(message);
 
         //inflo la vista
-        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_board, null);
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_note, null);
         builder.setView(viewInflated);//le paso la lista para que la embeba
 
-        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewBoard);
+        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewNote);
 
         builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 //trim() es para borrar los espacios al final, por si el usuario los dejara
-                String boardName = input.getText().toString().trim();
-                if(boardName.length() > 0)
-                    //createNew(boardName);
-                     System.out.println();
+                String note = input.getText().toString().trim();
+                if(note.length() > 0)
+                    createNewNote(note);
+
                 else
-                    Toast.makeText(getApplicationContext(), "The name is required to create a new Board", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "The note can't be empty", Toast.LENGTH_LONG).show();
             }
         });
         //aqui lo crea y lo enseña! (a la alerta)
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+
+
+    @Override
+    public void onChange(Board board) {
+
     }
 }
