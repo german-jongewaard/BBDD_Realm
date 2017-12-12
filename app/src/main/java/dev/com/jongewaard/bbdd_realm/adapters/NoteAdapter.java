1422dbe3f53e85c7d3040e447c2b18364f45890a
german <germanjongewaard@gmail.com>
2017-12-12 02:49:21 +0100

    BBDD Realm

--- app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/NoteAdapter.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/NoteAdapter.java
@@ -1,14 +1,17 @@
 package dev.com.jongewaard.bbdd_realm.adapters;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
-import dev.com.jongewaard.bbdd_realm.models.Board;
+import dev.com.jongewaard.bbdd_realm.R;
 import dev.com.jongewaard.bbdd_realm.models.Note;
 
 /**
@@ -30,21 +33,58 @@ public class NoteAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Note getItem(int position) {
+        return list.get(position);
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int id) {
+        return id;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder vh;
+
+        if(convertView == null) {
+            //inflo la vista con el layout que nos pasan del constructor
+            convertView = LayoutInflater.from(context).inflate(layout,null);
+            vh = new ViewHolder();
+            vh.descripcion = (TextView) convertView.findViewById(R.id.textViewNoteDescription);
+            vh.createdAt = (TextView) convertView.findViewById(R.id.textViewNotesCreatedAt);
+            //dentro del convertView paso el ViewHolder como un Tag
+            convertView.setTag(vh);
+        }else {
+            //lo casteo a ViewHolder porque estoy seguro que el convertView es un VH y
+            // no hace falta hacer un tr catch
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        //aqui cojo el objeto con el que voy a trabajar, el objeto trabaja en toda nuestra lista
+        Note notes = list.get(position); //lo cargo con los datos de nuestro modelo
+        vh.descripcion.setText(notes.getDescription());
+
+        int numberOfNotes = notes.getId();//numero de notas que voy a meter (agarro el numero)
+        //si el numero de notas es igual a uno, pongo en singular, sino en plural
+        String textForNotes = (numberOfNotes == 1) ? numberOfNotes + " Note" : numberOfNotes + " Notes";
+        vh.notes.setText(textForNotes);
+
+        //formateo de la fecha!
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String createdAt = df.format(board.getCreateAt());
+        vh.createdAt.setText(createdAt);//cojo la fecha y la paso a String
+
         return convertView;
+    }
+
+
+    public class ViewHolder {
+        TextView descripcion;
+        TextView createdAt;
     }
 }
