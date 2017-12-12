1fec4c0a4d17af4fb552e3de46654ec25f384b8f
german <germanjongewaard@gmail.com>
2017-12-12 02:39:46 +0100

    BBDD Realm

--- app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/NoteAdapter.java
+++ app/src/main/java/dev/com/jongewaard/bbdd_realm/adapters/NoteAdapter.java
@@ -1,9 +1,15 @@
 package dev.com.jongewaard.bbdd_realm.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import dev.com.jongewaard.bbdd_realm.models.Board;
+import dev.com.jongewaard.bbdd_realm.models.Note;
 
 /**
  * Created by german on 12-12-17.
@@ -12,11 +18,15 @@ import android.widget.BaseAdapter;
 public class NoteAdapter extends BaseAdapter {
 
 
+    private Context context;
+    private List<Note> list;
+    private int layout;
 
-
-
-
-
+    public NoteAdapter(Context context, List<Note> notes, int layout) {
+        this.context = context;
+        this.list = notes;
+        this.layout = layout;
+    }
 
     @Override
     public int getCount() {
